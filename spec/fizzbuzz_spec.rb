387bc1eb3cb37056c5043a88c26f7738d438d107
keomony khun <keomony@gmail.com>
2016-11-18 08:56:43 +0000

    outputs 'fizz' for multiples by 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -28,18 +28,22 @@ describe "Fizzbuzz" do
 
   context "random divisibility" do
 
-    it "should return true if number is divisible by divisor" do
+    it "should return true when number is divisible by divisor" do
       expect(is_divisible_by?(35,5)).to eq true
       expect(is_divisible_by?(9,3)).to eq true
       expect(is_divisible_by?(18,2)).to eq true
     end
 
-    it "should return false if number is divisible by divisor" do
+    it "should return false when number is divisible by divisor" do
       expect(is_divisible_by?(11,4)).to eq false
       expect(is_divisible_by?(97,7)).to eq false
       expect(is_divisible_by?(43,6)).to eq false
     end
 
+  end
+
+  it "should out put 'fizz' for multiples by 3" do
+    expect(fizzbuzz(3)).to eq 'fizz'
   end
 
 end
