bc1df018ecdbc402683e1f774e029994e2367bb4
keomony khun <keomony@gmail.com>
2016-11-18 08:26:44 +0000

    knows if the number whether or not is divisible by 15

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,7 @@
 require 'fizzbuzz'
+
 describe "Fizzbuzz" do
+
   it "should know when the number is divisible by 3" do
     expect(is_divisible_by_3?(3)).to eq true
   end
@@ -23,4 +25,5 @@ describe "Fizzbuzz" do
   it "should know when the nubmer is NOT divisible by 3 and 5" do
     expect(is_divisible_by_15?(16)).to eq false
   end
+  
 end
